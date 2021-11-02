177c1501eb196ffccb8881499aaf1e872af167ef
junyou0424 <junyou0424@gmail.com>
2021-11-02 22:49:24 +0800

    parsedata and verify function

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,7 @@ const packet_t = '000e39e3340000169c7cb00008004500006c000040003611623a16740741c9
 const header_t = '22.116.7.65201.174.250.22798984051517'
 const inport_t = '00:16:9c:7c:b0:00'
 const timestamp_t = '0.000000000'
-let p_src=0,p_dst=0
+let p_src='',p_dst=''
 
 main()
 
@@ -20,6 +20,7 @@ function Init() {
     let signature_info = Generate_SessionKey(VID + timestamp_t, secretKey)
     let packethash = Generate_PktHash(packet_t)
     p_src = packethash
+    p_dst = packethash
     /*console.log('VID\tLen:' + VID.length + '\n', VID)
     console.log('Packet\tLen:' + packethash.length + '\n', packethash)
     console.log('SecretKey\tLen:' + secretKey.length + '\n', secretKey)
@@ -27,7 +28,7 @@ function Init() {
     console.log('compress signature_key\tLen:' + signature_key.length + '\n', signature_key)
     console.log('compress signature_info\tLen:' + signature_info.length + '\n', signature_info)*/
     Tagging(VID, compress_key, packethash, signature_key, signature_info, 1)
-    Verify(p_src,p_dst)
+    Verify(p_src,p_dst,VID)
 }
 
 function Tagging(VID, compress_key, packethash, signature_key, signature_info, i) {
@@ -37,25 +38,28 @@ function Tagging(VID, compress_key, packethash, signature_key, signature_info, i
             alpha = [],
             tag = [],
             temp
+        //console.log('Packet\tLen:' + packethash.length + '\n', packethash)
         temp = Generate_PktHash(packethash)
+        p_dst += temp
+        console.log('temp\tLen:' + temp.length + '\n', temp)
         packethash = temp
         SK_a[i] = Generate_SessionKey(VID + 0, secretKey)
         SK_b[i] = Generate_SessionKey(VID + 1, secretKey)
         alpha[i] = Generate_SessionKey(SK_a[i], secretKey)
-        //console.log('compress alpha\tLen:' + alpha[i].length + '\n', alpha[i])
+        console.log('compress alpha\tLen:' + alpha[i].length + '\n', alpha[i])
         let a = Inner_Product(packethash, alpha[i])
         let b = Generate_SessionKey(SK_b[i], VID + i.toString())
         tag[i] = a + b
-        //console.log('tag\tLen:' + tag[i].length + '\n', tag[i])
-        //console.log('Packet\tLen:' + packethash.length + '\n', packethash)
+        console.log('tag\tLen:' + tag[i].length + '\n', tag[i])
         packethash += tag[i]
-        p_dst = packethash
         i++
         Tagging(VID, compress_key, packethash, signature_key, signature_info, i)
     }
 }
 
-function Verify(p_src,p_dst) {
+function Verify(p_src,p_dst,VID) {
+    console.log('p_src len:'+p_src.length+'\n'+p_src+'\n')
+    console.log('p_dst len:'+p_dst.length+'\n'+p_dst+'\n')
     for(let i=1;i<4;i++){
         let SK_a = [],
             SK_b = [],
@@ -63,8 +67,10 @@ function Verify(p_src,p_dst) {
         SK_a[i] = Generate_SessionKey(VID + 0, secretKey)
         SK_b[i] = Generate_SessionKey(VID + 1, secretKey)
         alpha[i] = Generate_SessionKey(SK_a[i], secretKey)
-        console.log(p_src+'\n')
-        console.log(p_dst+'\n')
+        let a = Inner_Product(p_dst.slice(i*40,i*40+40),alpha[i])
+        let b = Generate_SessionKey(SK_b[i], VID + i.toString())
+        tag = a+b
+        console.log('tag\n'+tag)
     }
 }
 
--- parsedata.js
+++ parsedata.js
@@ -5,7 +5,7 @@ let data = JSON.parse(rawdata)
 let flow_set = new Array()
 let flow = new Array()
 let set_count = 0
-for(let i=0;i<data.length;i++){
+for (let i = 0; i < data.length; i++) {
     flow[i] = new Object()
     flow[i].src_port = data[i]._source.layers.eth['eth.src']
     flow[i].dst_port = data[i]._source.layers.eth['eth.dst']
@@ -13,19 +13,29 @@ for(let i=0;i<data.length;i++){
     flow[i].ip_dst = data[i]._source.layers.ip['ip.dst']
     flow[i].proto = data[i]._source.layers.ip['ip.proto']
 }
-//console.log(flow)
 Flow_Classify(flow)
 
 function Flow_Classify(set) {
-    flow_set = set.filter(Compare)
-    //console.log(flow_set)
-}
-
-function Compare(set) {
-    for(let i = 0;i < set.length;i++){
-            for(let j=1;j<set.length-1;j++){
-                if(Object.is(set[i],set[j]))
-                    return set[i]
+    flow_set[set_count] = []
+    for (let i = 0; i < set.length-1; i++) {
+        if(set[i]!=0)
+            flow_set[set_count].push(data[i])
+        for (let j = i + 1; j < set.length; j++) {
+            if (Compare(set[i], set[j])) {
+                flow_set[set_count].push(data[j])
+                set[j] = 0
+            }
+            else if (j == set.length - 1) {
+                //console.log(flow_set[set_count],set_count)
+                set_count++
+                flow_set[set_count] = []
             }
         }
+    }
 }
+function Compare(a, b) {
+    if (a.src_port == b.src_port & a.dst_port == b.dst_port & a.ip_src == b.ip_src & a.ip_dst == b.ip_dst & a.proto == b.proto)
+        return true
+}
+
+console.log(flow_set)
